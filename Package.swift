041eb06fceecaee687d4bf6ff5fd6e389f319d6c
Demistry <dave11ilen@gmail.com>
2019-02-14 16:26:01 +0100

    Added dependency for PostgreSQL

--- Package.swift
+++ Package.swift
@@ -8,10 +8,14 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+        
+        //PostgreSQL dependency
+        .package(url: "https://github.com/vapor/postgresql.git", from: "1.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        //.target(name: "App", dependencies: ["PostgreSQL", ...]),
+        .target(name: "App", dependencies: ["PostgreSQL","FluentSQLite", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
